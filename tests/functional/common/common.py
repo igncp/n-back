78ccec9d4d5cb32f404378186c39aaf2b2d15285
Ignacio Carbajo <icarbajop@gmail.com>
2015-03-28 16:56:09 +0100

    Change NBackCommon class

--- tests/functional/common/common.py
+++ tests/functional/common/common.py
@@ -6,14 +6,17 @@ import unittest
 
 class NBackCommon(unittest.TestCase, selenium_base.SeleniumBase):
   def setUp(self):
-    self.start_server()
+    self.start_server_if_necessary()
     self.start_driver()
 
   def tearDown(self):
     self.close_browser()
     
-  def start_server(self):
+  def start_server_if_necessary(self):
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     result = sock.connect_ex(('127.0.0.1', 7171))
     if result != 0:
+      self.start_server()
+
+  def start_server(self):
       subprocess.Popen(['grunt', 'server'])
